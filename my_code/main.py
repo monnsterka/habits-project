d0021e20df188dfc1ffe52be286f8f190bd600ae
Monsterka <anule.machackova@seznam.cz>
2026-04-24 18:27:49 +0200

    delete habit (with the help of ID)

--- my_code/main.py
+++ my_code/main.py
@@ -30,6 +30,13 @@ class Database:
     def get_habits(self):
         self.cursor.execute("SELECT id, name FROM habits")
         return self.cursor.fetchall()
+    
+    def delete_habit(self, habit_id):
+        self.cursor.execute(
+            "DELETE FROM habits WHERE id = ? ",
+            (habit_id,)
+        )
+        self.conn.commit()
 
     def close(self):
         self.conn.close()
@@ -49,6 +56,7 @@ def menu():
     print("\n--- MENU ---")
     print("1 - create habit")
     print("2 - show habits")
+    print("3 - delete habit")
     print("0 - Quit")
     return input("Choose option: ")
 
@@ -67,6 +75,14 @@ def show_habits(db):
     else:
         for h in habits:
             print(f"{h[0]} - {h[1]}")
+
+def delete_habit(db):
+    try:
+        habit_id = int(input("Enter habit ID to delete:  "))
+        db.delete_habit(habit_id)
+        print("Habit deleted")
+    except ValueError:
+        print("Habit deleted")
 
 
 # MAIN
@@ -88,6 +104,9 @@ def main():
 
         elif choice == "2":
             show_habits(db)
+
+        elif choice == "3":
+            delete_habit(db)
 
         elif choice == "0":
             print("Ending the application")
